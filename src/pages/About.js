4ee51f8be9117ce3da0fcfe0b3663b50589b936b
Crypt0nic0 <n.greneche45@gmail.com>
2022-10-29 00:53:37 +0200

    project finished

---
+++ src/pages/About.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Logo from '../components/Logo';
+import Navigation from '../components/Navigation';
+
+const About = () => {
+    return (
+        <div>
+            <Logo />
+            <Navigation />
+            <h1>A propos</h1>
+            <br />
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium, earum exercitationem doloremque, beatae reiciendis voluptatum ipsum ea illo quaerat culpa nisi alias nesciunt nostrum dolorum quae eos consequatur, sunt quod qui porro omnis! Voluptatibus facilis esse obcaecati officia atque natus nam repellat molestiae, placeat, soluta dicta maxime quisquam labore totam ab dignissimos veniam voluptas magnam minus at perspiciatis quis quibusdam hic accusantium! Provident, nobis totam. Sed ad officiis aliquid ea non recusandae in, repudiandae reprehenderit, neque sint totam, adipisci commodi? Natus dolorum beatae, eligendi et, cupiditate in obcaecati vel autem debitis blanditiis nemo quibusdam, laborum illum dolor deserunt hic commodi!</p>
+            <br />
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis magni ea eveniet laboriosam? Minima natus iste reprehenderit commodi nobis quos delectus nulla dolorem, at magnam dolor eos modi nemo porro, perferendis fugit! Sequi aperiam iure magnam ullam quaerat consequuntur accusamus, eligendi quasi. Consequatur asperiores magni, ipsa amet mollitia a dignissimos excepturi? Temporibus reiciendis culpa aperiam aut pariatur, nostrum perspiciatis, magnam autem placeat quibusdam consequuntur labore soluta adipisci qui nobis atque aspernatur! Ad, id inventore eveniet commodi reprehenderit ratione quasi exercitationem hic repellendus minus ipsum deleniti atque recusandae accusantium saepe, officia explicabo nemo necessitatibus! Voluptas architecto deserunt doloremque laudantium dolore harum esse veritatis, autem expedita dignissimos nihil facilis. Error ex explicabo ratione laboriosam eos non numquam repellat facilis reprehenderit possimus quidem omnis, maiores impedit iste nihil incidunt laudantium unde dolorem ipsam eveniet placeat recusandae adipisci aut. Veniam amet deleniti, fuga, incidunt recusandae exercitationem dignissimos rem dolorem explicabo quam, qui ex deserunt unde tempora nemo. Voluptas nisi dolor, magnam illum commodi earum consectetur quidem maiores alias aliquid soluta odit fugiat et pariatur doloremque rerum voluptatem qui officiis facilis! Nisi laboriosam accusantium totam sed et ducimus nam reprehenderit odit, eos temporibus hic dolorum enim doloribus, architecto sunt eum suscipit. Error impedit excepturi quaerat.</p>
+        </div>
+    );
+};
+
+export default About;
